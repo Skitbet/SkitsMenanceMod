6e63d4144db9b6ae1b26b92f61ab92463b368595
Skitbet <skeeterld06@gmail.com>
2024-01-16 21:57:20 -0500

    setup 1.0.1

---
+++ SkitsMenace/Patches/RoundManagerPatch.cs
@@ -0,0 +1,140 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SkitsMenace.Patches
+{
+    [HarmonyPatch(typeof(RoundManager))]
+    internal class RoundManagerPatch
+    {
+
+        [HarmonyPatch(nameof(RoundManager.LoadNewLevel))]
+        [HarmonyPrefix]
+        static void ModifyLevel(ref SelectableLevel newLevel)
+        {
+            if (SkitsMenanceMod.instance.CurrentLevelDefaultRarities != null)
+            {
+                SkitsMenanceMod.instance.CurrentLevelDefaultRarities.Clear();
+                SkitsMenanceMod.instance.CurrentLevelDefaultRarities = null;
+            }
+            SkitsMenanceMod.instance.CurrentLevelDefaultRarities = GetDefaultRarities(newLevel);
+
+            if (!SkitsMenanceMod.instance.EnemyCached)
+            {
+                SkitsMenanceMod.instance.logger.LogInfo("Caching....");
+                SkitsMenanceMod.instance.EnemyCached = true;
+                SkitsMenanceMod.instance.IndoorEnemyCache = FixIndoorEnemySpawns();
+                SkitsMenanceMod.instance.OutdoorEnemyCache = FixOutDoorEnemySpawns();
+            }
+
+            newLevel.Enemies = SkitsMenanceMod.instance.IndoorEnemyCache;
+            newLevel.OutsideEnemies = SkitsMenanceMod.instance.OutdoorEnemyCache;
+
+
+     /*       foreach (SpawnableEnemyWithRarity spawnable in newLevel.Enemies)
+            {
+                SkitsMenanceMod.instance.logger.LogInfo(spawnable.enemyType.enemyName + " " + spawnable.rarity);
+            }
+
+            foreach (SpawnableEnemyWithRarity spawnable in newLevel.OutsideEnemies)
+            {
+                SkitsMenanceMod.instance.logger.LogInfo(spawnable.enemyType.enemyName + " " + spawnable.rarity);
+            }*/
+
+        }
+
+        internal static Dictionary<string, int> GetDefaultRarities(SelectableLevel newLevel)
+        {
+            Dictionary<string, int> toReturn = new Dictionary<string, int>();
+
+            foreach (SpawnableEnemyWithRarity spawnable in newLevel.Enemies)
+            {
+                toReturn.Add(spawnable.enemyType.enemyName, (spawnable.rarity <= 5 ? UnityEngine.Random.Range(6, 100) : spawnable.rarity));
+            }
+
+            foreach (SpawnableEnemyWithRarity spawnable in newLevel.OutsideEnemies)
+            {
+                toReturn.Add(spawnable.enemyType.enemyName, (spawnable.rarity <= 5 ? UnityEngine.Random.Range(6, 100) : spawnable.rarity));
+            }
+
+            return toReturn;
+        }
+
+        internal static List<SpawnableEnemyWithRarity> FixIndoorEnemySpawns()
+        {
+            SkitsMenanceMod.instance.logger.LogInfo("Caching indoor enemies.");
+            List<SpawnableEnemyWithRarity> returnList = new List<SpawnableEnemyWithRarity>();
+
+            returnList.Add(getEntitySpawner<JesterAI>());
+            returnList.Add(getEntitySpawner<DressGirlAI>());
+            returnList.Add(getEntitySpawner<SandSpiderAI>());
+            returnList.Add(getEntitySpawner<CrawlerAI>());
+            returnList.Add(getEntitySpawner<BlobAI>());
+            returnList.Add(getEntitySpawner<CentipedeAI>());
+            returnList.Add(getEntitySpawner<FlowermanAI>());
+            returnList.Add(getEntitySpawner<HoarderBugAI>());
+            returnList.Add(getEntitySpawner<LassoManAI>());
+            returnList.Add(getEntitySpawner<PufferAI>());
+            returnList.Add(getEntitySpawner<SpringManAI>());
+            returnList.Add(getEntitySpawner<NutcrackerEnemyAI>());
+
+            SkitsMenanceMod.instance.logger.LogInfo("Cached indoor enemies.");
+
+            return returnList;
+        }
+
+        internal static List<SpawnableEnemyWithRarity> FixOutDoorEnemySpawns()
+        {
+            SkitsMenanceMod.instance.logger.LogInfo("Caching outdoor enemies.");
+
+            List<SpawnableEnemyWithRarity> returnList = new List<SpawnableEnemyWithRarity>();
+
+            returnList.Add(getEntitySpawner<DressGirlAI>());
+            returnList.Add(getEntitySpawner<SandWormAI>());
+            returnList.Add(getEntitySpawner<MouthDogAI>());
+            returnList.Add(getEntitySpawner<BaboonBirdAI>());
+            returnList.Add(getEntitySpawner<DocileLocustBeesAI>());
+            returnList.Add(getEntitySpawner<DoublewingAI>());
+            returnList.Add(getEntitySpawner<ForestGiantAI>());
+
+            SkitsMenanceMod.instance.logger.LogInfo("Cached outdoor enemies.");
+
+            return returnList;
+        }
+        internal static SpawnableEnemyWithRarity getEntitySpawner<T>() where T : EnemyAI
+        {
+            T refToEnemy = null;
+            foreach (UnityEngine.Object o in Resources.FindObjectsOfTypeAll(typeof(T)))
+            {
+                refToEnemy = (T)o;
+            }
+            SpawnableEnemyWithRarity spawnable = new SpawnableEnemyWithRarity();
+
+            if (refToEnemy != null)
+            {
+ /*               foreach (var pair in SkitsMenanceMod.instance.CurrentLevelDefaultRarities)
+                {
+                    Console.WriteLine($"Enemy Type: {pair.Key.name}, Value: {pair.Value}");
+                }*/
+                spawnable.enemyType = refToEnemy.enemyType;
+                int rarity;
+                // for some reason enemies arent in rarities sometimes
+                if (SkitsMenanceMod.instance.CurrentLevelDefaultRarities.ContainsKey(refToEnemy.enemyType.enemyName))
+                {
+                    rarity = SkitsMenanceMod.instance.CurrentLevelDefaultRarities[refToEnemy.enemyType.enemyName];
+                } 
+                else
+                {
+                    SkitsMenanceMod.instance.logger.LogInfo("Couldn't find cached rareity for this level for the " + spawnable.enemyType.enemyName + ", defaulting to random value.");
+                    rarity = UnityEngine.Random.Range(6, 100);
+                }
+                spawnable.rarity = rarity;
+            }
+            return spawnable;
+        }
+    }
+}
---
+++ SkitsMenace/SkitsMenanceMod.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using SkitsMenace.Patches;
+using System.Collections.Generic;
+
+namespace SkitsMenace
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class SkitsMenanceMod : BaseUnityPlugin
+    {
+        private const string modGUID = "Skitbet.SkitsMenance";
+        private const string modName = "SkitsMenance";
+        private const string modVersion = "1.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static SkitsMenanceMod instance { get; private set; }
+
+        internal ManualLogSource logger;
+
+        internal List<SpawnableEnemyWithRarity> IndoorEnemyCache;
+        internal List<SpawnableEnemyWithRarity> OutdoorEnemyCache;
+        internal Dictionary<string, int> CurrentLevelDefaultRarities;
+
+
+        internal bool EnemyCached; // doing this so we dont recache every new round
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            logger.LogInfo("Patching SkitsMenance Mod...");
+
+            harmony.PatchAll(typeof(RoundManagerPatch));
+
+            logger.LogInfo("Patched SkitsMenance Mod!");
+
+
+        }
+
+    }
+}
